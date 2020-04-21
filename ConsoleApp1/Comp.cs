8e64e40297954f5c45ee4b9fdebd1af3151b86da
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-04-21 23:32:50 +0400

    I will go crazy...

---
+++ ConsoleApp1/Comp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace IdleGame
+{
+    class Comp
+    {
+        public readonly int[] CompNumber = new int[4];
+        private Random rand = new Random();
+        public Comp()
+        {
+            CompNumber[0] = rand.Next(1, 10);
+            CompNumber[1] = Initialization_Besides(CompNumber[0]);
+            CompNumber[2] = Initialization_Besides(CompNumber[0], CompNumber[1]);
+            CompNumber[3] = Initialization_Besides(CompNumber[0], CompNumber[1], CompNumber[2]);
+        }
+        private int Initialization_Besides(int num)
+        {
+            int a = rand.Next(1, 10);
+            while(a == num)
+            {
+                a = rand.Next(1, 10);
+            }
+            return a;
+        }
+        private int Initialization_Besides(int num1, int num2)
+        {
+            int a = rand.Next(1, 10);
+            while (a == num1 || a == num2)
+            {
+                a = rand.Next(1, 10);
+            }
+            return a;
+        }
+        private int Initialization_Besides(int num1, int num2, int num3)
+        {
+            int a = rand.Next(1, 10);
+            while (a == num1 || a == num2 || a == num3)
+            {
+                a = rand.Next(1, 10);
+            }
+            return a;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace IdleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Համակարգը գեներացնում է քառանիշ թիվ
+            //User-ը գրում է իր քառանիշ թիվը
+            //Համակարգը գրում է համնկնող թվերի քանակը և դրանց դիրքերը
+
+            Game.GameDisplay();
+
+            Console.ReadLine();
+        }
+    }
+}
